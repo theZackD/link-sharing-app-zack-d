f1299d919303813b98bc4afef97fb34326a8e396
theZackD <dr.zakaria95@gmail.com>
2023-10-26 00:18:43 +0200

    login page done and styles

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,4 +1,5 @@
 import LogoLarge from './assets/images/logo-devlinks-large.svg'
+import EmailIcon from './assets/images/icon-email.svg'
 import './Login.css'
 import './components.css'
 
@@ -11,14 +12,16 @@ const Login = () => {
         <div className="Log-container">
           <div className='title'>
               <h2>Login</h2>
-              <p>Add your details below to get back into the app</p>
+              <p id='description'>Add your details below to get back into the app</p>
           </div>
           <form action="#">
-              <label htmlFor="Email">Email</label>
-              <input type="text" name='Email' placeholder='e.g.: alex@email.com' />
+              <label htmlFor="Email">Email address</label>
+              <input type="text" name='Email' placeholder={`e.g.: alex@email.com`} />
               <label htmlFor="Password">Password</label>
               <input type="text" name='Password' placeholder='Enter your password' />
+              <button>Login</button>
           </form>
+          <p id='createacc'>Don't have an account ? <a href="">Create an account</a></p>
         </div>
     </div>
   )
